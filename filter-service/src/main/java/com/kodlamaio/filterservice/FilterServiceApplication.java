03af5507fe3b046fc1b4f0ed6ffa7068954a99b3
Nur Yücel <nrycl7@gmail.com>
2023-05-07 19:04:44 +0300

    api gateway added

--- filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
@@ -2,12 +2,14 @@ package com.kodlamaio.filterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FilterServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilterServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FilterServiceApplication.class, args);
+    }
 
 }
