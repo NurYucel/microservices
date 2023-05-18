73ccd0959fb29a7a4a8f529a63233ecccf2c81a8
Nur Yücel <nrycl7@gmail.com>
2023-05-18 20:12:10 +0300

    latest

--- rental-service/src/main/java/com/example/rentalservice/repository/RentalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rentalservice.repository;
-
-import com.example.rentalservice.entities.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RentalRepository extends JpaRepository<Rental, UUID> {
-}
---
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/business/abstracts/RentalService.java
@@ -0,0 +1,23 @@
+package com.kodlamaio.rentalservice.business.abstracts;
+
+import com.kodlamaio.rentalservice.business.dto.requests.CreateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.requests.UpdateRentalRequest;
+import com.kodlamaio.rentalservice.business.dto.response.CreateRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.response.GetAllRentalsResponse;
+import com.kodlamaio.rentalservice.business.dto.response.GetRentalResponse;
+import com.kodlamaio.rentalservice.business.dto.response.UpdateRentalResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface RentalService {
+    List<GetAllRentalsResponse> getAll();
+
+    GetRentalResponse getById(UUID id);
+
+    CreateRentalResponse add(CreateRentalRequest request);
+
+    UpdateRentalResponse update(UUID id, UpdateRentalRequest request);
+
+    void delete(UUID id);
+}
