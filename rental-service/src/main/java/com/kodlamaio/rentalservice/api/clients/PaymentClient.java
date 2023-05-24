5d6b26b43f6b4348904d41db114d238bf902f6f4
Nur Yücel <nrycl7@gmail.com>
2023-05-24 22:31:28 +0300

    add config-server

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/PaymentClient.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/PaymentClient.java
@@ -3,13 +3,13 @@ package com.kodlamaio.rentalservice.api.clients;
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
 import com.kodlamaio.commonpackage.utils.dto.CreateRentalPaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "payment-service", fallback = PaymentClientFallback.class)
 public interface PaymentClient {
     //TODO: check
-    @GetMapping(value = "/api/payments/process")
-    ClientResponse processRentalPayment(@PathVariable CreateRentalPaymentRequest request);
+    @PostMapping(value = "/api/payments/process")
+    ClientResponse processRentalPayment(@RequestBody CreateRentalPaymentRequest request);
 
 }
